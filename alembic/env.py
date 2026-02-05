c997798fd34c771318dfb4da72f024feb496f982
Devin Thai <devin.thai.5@gmail.com>
2026-02-04 21:08:35 -0800

    added slugs and metadata_snapshots to the first database change

--- alembic/env.py
+++ alembic/env.py
@@ -20,8 +20,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from db.models import slugs
-target_metadata = slugs.Base.metadata
+# from db.models import slugs, metadata_snapshot
+from db.models import slugs, metadata_snapshot
+from db.base import Base
+target_metadata = Base.metadata 
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
